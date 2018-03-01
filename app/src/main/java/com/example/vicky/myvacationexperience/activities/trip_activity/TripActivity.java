7860a9ba831137464f55984d7dd5ba5deed318c3
Vicky <gmariavictoria91@gmail.com>
2018-03-01 00:52:29 -0300

    Boton sin funcionamiento de map, tentativa de menu de places

--- app/src/main/java/com/example/vicky/myvacationexperience/activities/trip_activity/TripActivity.java
+++ app/src/main/java/com/example/vicky/myvacationexperience/activities/trip_activity/TripActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.vicky.myvacationexperience.R;
@@ -35,6 +36,13 @@ public class TripActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_layers, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
             case android.R.id.home:
@@ -42,7 +50,9 @@ public class TripActivity extends AppCompatActivity {
                 this.finish();
                 return true;
 
-
+            case R.id.actionMap:
+                //TODO llamar al nuevo action del Mapa
+                return true;
 
             default:
                 return false;
--- app/src/main/java/com/example/vicky/myvacationexperience/activities/trip_list_activity/TripListActivity.java
+++ app/src/main/java/com/example/vicky/myvacationexperience/activities/trip_list_activity/TripListActivity.java
@@ -50,13 +50,13 @@ public class TripListActivity extends AppCompatActivity {
 
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+*/
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
