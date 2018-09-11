89d1f6dd8e9a478a627726dd42b3b4d9dd845ba4
vickyzalez <gmariavictoria91@gmail.com>
2018-09-11 00:54:20 -0300

    Validaciones al crear una nueva categoria

--- app/src/main/java/com/wikitude/sdksamples/dialogs/NewLayerDialogFragment.java
+++ app/src/main/java/com/wikitude/sdksamples/dialogs/NewLayerDialogFragment.java
@@ -5,6 +5,7 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -208,60 +209,93 @@ public class NewLayerDialogFragment extends DialogFragment implements View.OnCli
 
             case R.id.btnNewLayerOk:
 
-                if (layerModify == null){
-
-                    LayerTrip layer = new LayerTrip(this.layerName.getText().toString(),this.icon,true);
-                    int index = (control.getLayers().size());
-                    control.getLayers().add(layer);
-                    LayerView layerViewAdapter = new LayerView(control, activity, layer, tripModify, index);
-                    if(control.getAdapterList() == null || control.getAdapterList().isEmpty()){
-                        List<LayerView> arrayList = new ArrayList<>();
-                        arrayList.add(layerViewAdapter);
-                        control.setAdapterList(arrayList);
-                    } else {
-                        control.getAdapterList().add(layerViewAdapter);
-                    }
-
-                    //se guarda en el celu
-                    try {
-                        FileHandler.saveTrip(control.getTrip(), activity);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-
-                }
-
-                //Actualiza activity
-                control.updateList(layer, null);
-
+                if(this.layerName.getText().toString().equals("")){
+                    new AlertDialog.Builder(activity)
+                            .setTitle(R.string.errorTitle)
+                            .setMessage(R.string.emptyFieldsMsg)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .setPositiveButton(R.string.btnOk, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {}})
+                            .show();
                 } else {
 
-                    layerModify.setName(this.layerName.getText().toString());
-                    layerModify.setIcon(this.icon);
+                    if(duplicatedLayer()){
+                        new AlertDialog.Builder(activity)
+                                .setTitle(R.string.errorTitle)
+                                .setMessage(R.string.duplicatedLayer)
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .setPositiveButton(R.string.btnOk, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {}})
+                                .show();
 
-                    for (LayerTrip layerTrip: tripModify.getLayers()){
-                        if (layerTrip.getName().toString().equals(layerModify.getName().toString())){
-                            layerTrip = layerModify;
-                        }
-                    }
-                    //se guarda en el celu
-                    try {
-                        FileHandler.saveTrip(tripModify, activity);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    } else {
 
-                    //Actualiza activity
-                    control.updateList(layerModify, positionLayer);
+                        if (layerModify == null){
 
-            }
+                                LayerTrip layer = new LayerTrip(this.layerName.getText().toString(),this.icon,true);
+                                int index = (control.getLayers().size());
+                                control.getLayers().add(layer);
+                                LayerView layerViewAdapter = new LayerView(control, activity, layer, tripModify, index);
+                                if(control.getAdapterList() == null || control.getAdapterList().isEmpty()){
+                                    List<LayerView> arrayList = new ArrayList<>();
+                                    arrayList.add(layerViewAdapter);
+                                    control.setAdapterList(arrayList);
+                                } else {
+                                    control.getAdapterList().add(layerViewAdapter);
+                                }
+
+                                //se guarda en el celu
+                                try {
+                                    FileHandler.saveTrip(control.getTrip(), activity);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+
+                                }
+
+                                //Actualiza activity
+                                control.updateList(layer, null);
+
+                                } else {
+
+
+
+                                layerModify.setName(this.layerName.getText().toString());
+                                layerModify.setIcon(this.icon);
+
+                                for (LayerTrip layerTrip : tripModify.getLayers()) {
+                                    if (layerTrip.getName().toString().equals(layerModify.getName().toString())) {
+                                        layerTrip = layerModify;
+                                    }
+                                }
+                                //se guarda en el celu
+                                try {
+                                    FileHandler.saveTrip(tripModify, activity);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                                //Actualiza activity
+                                control.updateList(layerModify, positionLayer);
+                                }
+                            }
 
                 this.dismiss();
                 break;
+            }
         }
+    }
+
+    private boolean duplicatedLayer() {
+        for (LayerTrip layer: control.getTrip().getLayers()){
+            if (layer.getName().equals(this.layerName.getText().toString())){
+                return true;
+            }
+        }
+        return false;
     }
 
     private void changeBackground(){
